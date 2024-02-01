22d141f2aad23e6b02e2fbfab8ad9ffe62ae532c
sabbbirr <sabbir.ahmed1925@gmail.com>
2024-02-02 01:22:48 +0600

    removed MNIST and added gitignore

--- W_Pytorch/p2_datasets.py
+++ W_Pytorch/p2_datasets.py
@@ -2,12 +2,15 @@ import torch
 import torchvision
 from torchvision import transforms, datasets 
 
-train = datasets.MNIST("", train=True, download = True,
-                       transform = transforms.Compose([transforms.ToTensor]))
+transform = transforms.Compose([transforms.ToTensor()])
 
-test = datasets.MNIST("", train=False, download = True,
-                      transform = transforms.Compose([transforms.ToTensor]))
+train = datasets.MNIST("", train=True, download = True, transform = transform)
+test = datasets.MNIST("", train=False, download = True, transform = transform)
 
 trainset = torch.utils.data.DataLoader(train, batch_size = 10, shuffle = True)
 testset = torch.utils.data.DataLoader(test, batch_size = 10, shuffle = True)
 
+
+for data in trainset:
+    print(data)
+    break
